efdc734d8e26b44bd647f2c93f4a1aa82b46fd2f
tedschu <ted.schuster@gmail.com>
2024-06-17 11:24:40 -0500

    adding nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,12 @@
 // Persistent nav bar with logo / site name and user icon
+import { Link } from "react-router-dom";
 
 function Nav() {
-  return <></>;
+  return (
+    <>
+      <h1>This is the nav bar</h1>
+    </>
+  );
 }
 
 export default Nav;
