3b7b7a9dbcfb107dfe5d51fbe712b959a5a43852
tedschu <ted.schuster@gmail.com>
2024-06-18 11:59:46 -0500

    updating products css and nav files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,23 @@
 // Persistent nav bar with logo / site name and user icon
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import logo from "../assets/logo.jpeg";
 
 function Nav() {
   return (
     <>
-      <h1>This is the nav bar</h1>
+      <nav>
+        <div></div>
+        <Link to={"/"} className="logo">
+          <img src={logo} />
+        </Link>
+        <div className="navLinks">
+          <Link to={"/login"} className="userLogo">
+            <FontAwesomeIcon icon={faUser} size={"2x"} />
+          </Link>
+        </div>
+      </nav>
     </>
   );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,14 +3,92 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
-// import any other dependent files here (ex. checkout)
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [noSearchResults, setNoSearchResults] = useState(false);
+  const [value, setValue] = useState("");
+
+  // Grabs products from the database, updates products and filteredProducts states
+  useEffect(() => {
+    async function getAllProducts() {
+      const response = await fetch("http://localhost:8080/api/products/", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setProducts(data);
+      setFilteredProducts(data);
+    }
+    getAllProducts();
+  }, []);
+
+  //console.log(filteredProducts[0].product_name);
+
+  // Updates filteredProducts when there are search results
+  useEffect(() => {
+    const searchResultArray = products.filter((product) =>
+      product.product_name.toLowerCase().includes(value.toLowerCase())
+    );
+    //console.log(searchResultArray);
+
+    setFilteredProducts(searchResultArray);
+    searchResultArray.length === 0 && setNoSearchResults(true); // TRIGGERS "NO RESULTS" TEXT BASED ON WHETHER RESULT ARRAY IS EMPTY OR NOT
+    searchResultArray.length > 0 && setNoSearchResults(false);
+  }, [value, products]);
+
+  // Search bar results
+  const setResults = (e) => {
+    setValue(e.target.value);
+    //console.log(value);
+  };
+
   return (
     <>
-      <div className="productContainer">
-        <h1>This is the product page</h1>
+      <div className="searchContainer">
+        <div className="searchBar">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            size={"1x"}
+            className="searchIcon"
+          />
+          <input
+            type="text"
+            placeholder="Search for your gear..."
+            onChange={setResults}
+          />
+        </div>
+      </div>
+      <div className="productPageContainer">
+        {/* <div className="leftMenu">This is the selecton menu</div> */}
+        <div className="productListContainer">
+          {noSearchResults && (
+            <h2>There are no products that match your search</h2>
+          )}
+          {filteredProducts.map((product) => (
+            <Link
+              className="link"
+              to={"products/" + product.product_id}
+              key={product.product_id}
+            >
+              <div className="productContainer">
+                <h2>{product.product_name}</h2>
+                {/* <h3>{product.product_description}</h3> */}
+                <h2 className="price">
+                  {product.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </h2>
+                <img src={product.image_url} alt={product.product_name} />
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
